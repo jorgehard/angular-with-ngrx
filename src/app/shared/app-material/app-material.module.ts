957f3286ef3df96ef4b06b92e5c8fe1357c06372
Jorge H Baptista <jorgehenrique@live.com>
2023-03-25 00:28:18 -0300

    implements dashbaord structure init

--- src/app/shared/app-material/app-material.module.ts
+++ src/app/shared/app-material/app-material.module.ts
@@ -4,9 +4,16 @@ import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @NgModule({
-  exports: [MatTableModule, MatCardModule, MatToolbarModule, MatButtonModule, MatIconModule],
+  exports: [
+    MatTableModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatGridListModule,
+  ],
 })
 export class AppMaterialModule {}
