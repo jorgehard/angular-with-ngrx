3585563104d4ecc96ad673fc195ea7903740160c
Jorge H Baptista <jorgehenrique@live.com>
2023-03-25 00:15:40 -0300

    adjust basic navbar

--- src/app/shared/app-material/app-material.module.ts
+++ src/app/shared/app-material/app-material.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @NgModule({
-  exports: [MatTableModule, MatCardModule, MatToolbarModule],
+  exports: [MatTableModule, MatCardModule, MatToolbarModule, MatButtonModule, MatIconModule],
 })
 export class AppMaterialModule {}
